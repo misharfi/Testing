e89fc243aa444b8bd6fb8195a1586a6e0c34332b
Sharfi <Sharfi@192.168.0.9>
2018-03-31 23:07:50 +0530

    Updated by Second Sharfi

--- LearningGithub/src/PackA/Test1.java
+++ LearningGithub/src/PackA/Test1.java
@@ -8,6 +8,8 @@ public class Test1 {
 	{
 		System.out.println("First Branch commit...............");
 		System.out.println("Branch committed by Second Sharfi...............");
+		System.out.println("Branch updated by Second Sharfi...............");
+		System.out.println("Branch updated and commited by Second Sharfi...............");
 		
 		
 	}
